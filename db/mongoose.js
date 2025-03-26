e6b0e9652e5dc720ac787901342fb2cf6146ed3b
SAQLAIN <saqlain@SAQLAINs-MacBook-Pro.local>
2025-03-27 01:03:42 +0530

    my all added

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI)
+// Set strictQuery to true to suppress the deprecation warning
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('Error connecting to MongoDB:', error));
